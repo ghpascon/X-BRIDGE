994266c9cd04b91b1d436acb0484806b9cc7cafb
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-01-16 14:18:47 -0300

    Add clear old tags async function

--- app/async_func/rfid.py
+++ app/async_func/rfid.py
@@ -1,8 +1,24 @@
 from app.services import rfid_manager
 import logging
-
+import asyncio
+from app.core import settings
+from datetime import datetime, timedelta
 
 async def connect_on_startup():
 	"""Connect to RFID devices on application startup."""
 	logging.info('Connecting to RFID devices on startup...')
 	await rfid_manager.devices.connect_devices()
+
+async def clear_old_tags():
+	while True:
+		if settings.CLEAR_OLD_TAGS_INTERVAL is None:
+			await asyncio.sleep(60)
+		await asyncio.sleep(settings.CLEAR_OLD_TAGS_INTERVAL)  
+		if len(rfid_manager.events.tags) == 0:
+			continue
+		
+		timestamp = datetime.now() - timedelta(seconds=settings.CLEAR_OLD_TAGS_INTERVAL)
+		logging.info(f"Removing tags before {timestamp}")
+		rfid_manager.events.tags.remove_tags_before_timestamp(timestamp)
+
+		
